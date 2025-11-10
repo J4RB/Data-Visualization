a86cb470ac7b4ea47179fa0324d91b4a2c03afb9
Platon Dimitriadis <platonasdim@gmail.com>
2025-11-10 08:54:20 +0100

    Heatmap for number of houses to house types with variables. Needs more work

--- server.R
+++ server.R
@@ -2,10 +2,12 @@
 source("R/modules/intro.R")
 source("R/modules/visualization.R")
 source("R/modules/map.R")
+source("R/modules/heatmap.R")
 
 server <- function(input, output, session) {
   # Call each module’s server logic
   intro_server("intro", data)
   visualization_server("visualization", data)
   map_server("map", data, dk_zip_sf, dk_region_sf)
+  heatmap_server("heatmap_vis", data)
 }
--- ui.R
+++ ui.R
@@ -2,11 +2,13 @@
 source("R/modules/intro.R")
 source("R/modules/visualization.R")
 source("R/modules/map.R")
+source("R/modules/heatmap.R")
 
 ui <- navbarPage(
   title = "[Title]",
   
   intro_ui("intro"),
   visualization_ui("visualization"),
-  map_ui("map")
+  map_ui("map"),
+  heatmap_ui("heatmap_vis")
 )
