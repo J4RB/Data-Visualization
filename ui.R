da6c289847701951590f0e88c46a93093a8158cc
Jacob Bregndahl Larsen <jlarsen2107@gmail.com>
2025-11-10 10:04:22 +0100

    Removed empty vizualization tab

--- ui.R
+++ ui.R
@@ -2,7 +2,6 @@
 source("R/modules/intro.R")
 source("R/modules/purchase_trand.R")
 source("R/modules/price_index.R")
-source("R/modules/visualization.R")
 source("R/modules/map.R")
 source("R/modules/data_set.R")
 source("R/modules/animation.R")
@@ -13,7 +12,6 @@ ui <- navbarPage(
   intro_ui("intro"),
   purchase_ui("purchase"),
   price_index_ui("price_index"),
-  visualization_ui("visualization"),
   animation_ui("animation"),
   map_ui("map"),
   data_set_ui("data_set")
